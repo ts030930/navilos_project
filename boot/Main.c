2007f6cb47b19a9482f2da5c950ccc9541f40ab8
Park Tae-sun <ts930@dgu.ac.kr>
2026-06-20 17:01:11 +0900

    ~ context switching

--- boot/Main.c
+++ boot/Main.c
@@ -1,14 +1,25 @@
 #include "stdint.h"
 #include "stdbool.h"
+
 #include "HalUart.h"
 #include "HalInterrupt.h"
-#include "stdio.h"
 #include "HalTimer.h"
+
+#include "stdio.h"
 #include "stdlib.h"
 
+#include "Kernel.h"
+
+static void Kernel_init(void);
 static void Hw_init(void);
 static void Printf_test(void);
 static void Timer_test(void);
+
+void User_task0(void);
+void User_task1(void);
+void User_task2(void);
+
+
 void main(void)
 {
     Hw_init();
@@ -25,6 +36,8 @@ void main(void)
     Printf_test();
 
     Timer_test();
+
+    Kernel_init();
 
     while(true);
 }
@@ -54,9 +67,70 @@ static void Printf_test(void)
 
 static void Timer_test(void)
 {
-    while(true)
+    for(uint32_t i = 0; i < 5 ; i++)
     {
         debug_printf("current count : %u\n", Hal_timer_get_1ms_counter());
         delay(1000);
+    }
+}
+
+static void Kernel_init(void)
+{
+    uint32_t taskId;
+    Kernel_task_init();
+    taskId = Kernel_task_create(User_task0);
+    if (NOT_ENOUGH_TASK_NUM == taskId)
+    {
+        putstr("Task0 creation fail\n");
+    }
+
+    taskId = Kernel_task_create(User_task1);
+    if (NOT_ENOUGH_TASK_NUM == taskId)
+    {
+        putstr("Task1 creation fail\n");
+    }
+
+    taskId = Kernel_task_create(User_task2);
+    if (NOT_ENOUGH_TASK_NUM == taskId)
+    {
+        putstr("Task2 creation fail\n");
+    }
+
+    Kernel_start();
+}
+
+void User_task0(void)
+{
+    uint32_t local = 0;
+
+    while(true)
+    {
+        debug_printf("User Task #0 sp=0x%x\n",&local);
+        Kernel_yield();
+    }
+    
+
+    
+}
+
+void User_task1(void)
+{
+     uint32_t local = 0;
+
+    while(true)
+    {
+        debug_printf("User Task #1 sp=0x%x\n",&local);
+        Kernel_yield();
+    }
+}
+
+void User_task2(void)
+{
+    uint32_t local = 0;
+
+    while(true)
+    {
+        debug_printf("User Task #2 sp=0x%x\n",&local);
+        Kernel_yield();
     }
 }
